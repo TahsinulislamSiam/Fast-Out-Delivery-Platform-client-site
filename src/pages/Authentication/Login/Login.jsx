1c19903d8e8a4711d18479e8a702636423260ce3
Tahsinul islam <mohammadsiam8760@gmail.com>
2026-01-19 17:51:25 +0600

    adding registration from

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const Login = () => {
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, formState:{errors} } = useForm()
 
     const onSubmit = data =>{
         console.log(data)
@@ -11,7 +11,7 @@ const Login = () => {
     return (
         <div>
            <form onSubmit={handleSubmit(onSubmit)}>
-            
+
             <fieldset className="fieldset">
           <label className="label">Email</label>
           <input type="email"
@@ -24,9 +24,17 @@ const Login = () => {
          {required:true,minLength:8})}
            className="input" placeholder="Password" />
 
+           {
+            errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>
+           }
+           {
+            errors.password?.type ==='minLength' && <p className='text-red-500'>password need atleast 8 character !!</p>
+           }
+
           <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          
         </fieldset>
+        <button className="btn btn-neutral mt-4">Login</button>
            </form>
         </div>
     );
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
 import AuthLayout from "../layouts/AuthLayout";
-import Login from "../pages/Authentication/Login";
+import Register from "../pages/Authentication/Register/Register";
+import Login from "../pages/Authentication/Login/Login";
 
 
 
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
       {
         path:'login',
         Component:Login
+      },
+      {
+        path:'register',
+        Component:Register
       }
     ]
   }
